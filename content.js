26d35e7bdb14cd284f41df3a59cd7ac2b8ad0fcb
Pavan7730 <82305384+Pavan7730@users.noreply.github.com>
2026-02-14 12:23:46 +0530

    Update content.js

--- content.js
+++ content.js
@@ -1,14 +1,26 @@
 (() => {
-  const getMeta = (name) =>
-    document.querySelector(`meta[name="${name}"]`)?.content || "Not specified";
+  const getMeta = (name, prop = false) => {
+    if (prop) {
+      return document.querySelector(`meta[property="${name}"]`)?.content || null;
+    }
+    return document.querySelector(`meta[name="${name}"]`)?.content || null;
+  };
 
   const data = {
-    title: document.title || "Not specified",
-    description: getMeta("description"),
-    robots: getMeta("robots"),
-    canonical:
-      document.querySelector('link[rel="canonical"]')?.href || "Not specified",
-    url: window.location.href
+    title: document.title || "",
+    description: getMeta("description") || "",
+    robots: getMeta("robots") || "Not specified",
+    canonical: document.querySelector('link[rel="canonical"]')?.href || "",
+    url: window.location.href,
+
+    ogImage: getMeta("og:image", true),
+
+    headings: {
+      h1: document.querySelectorAll("h1").length,
+      h2: document.querySelectorAll("h2").length,
+      h3: document.querySelectorAll("h3").length,
+      h4: document.querySelectorAll("h4").length
+    }
   };
 
   chrome.runtime.sendMessage(data);
